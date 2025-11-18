50bf1704364336c1436fa56d19e1008a28770ea2
Dillow <madushanka20021224@gmail.com>
2025-11-19 01:58:21 +0530

    creating loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 
+import { useState } from 'react';
 import './App.css'
 import { LoadingScreen } from './components/Loadingscreen';
 import "./index.css";
 
 function App() {
-  return(
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  return (
     <>
-      <LoadingScreen />
+    {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
     </>
-  )
+  );
 }
 
 export default App;
